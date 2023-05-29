4756136264d1198ab7a5ef072d9b50d4b0573805
JesseKefa <72498295+JesseKefa@users.noreply.github.com>
2023-05-29 08:56:24 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,56 +1,29 @@
-<DOCTYPE html>
+<?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+	$user_data = check_login($con);
+
+?>
     <html>
-        <head> 
-            <meta http-equiv="X-UA-Compatible" content="">
-            <meta charset="utf-8">
-            <title>Sign in page</title>
-            <link rel="stylesheet" type = "text/css" href="bootstrap.css">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/holiday.css" />
-        </head>
+    <head>
+    <title>Patient Login</title>
+    </head>
+    <body>
+        <h1>Patient Page</h1>
 
-        <body>
-            </style>
-            <div class = "container">
-                <div class = "row">
-                    <h2>Sign in</h2>
-                    <div class = "card">
-                        <div class = "card-body">
-                            <form action="process.php" method= "post">
-                                <div>
-                                <label for = "SSN" >SSN</label>
-                                <input type = "SSN" id = "SSN" name = "SSN">
-                                </div>
-                                <div>
-                                    <label for = "name" >Name</label>
-                                    <input type = "name" id =  "name" name = "name">
-                                </div>
-                                <div>
-                                    <label for ="address">Address</label>
-                                    <input type ="address" id = "address" name ="address">
-                                </div>
-                                <div>
-                                    <label for = "age" >Age</label>
-                                    <input type = "age " id = "age " name = "age ">
-                                </div>
-                                <div>
-                                    <label for = Password >Password</label>
-                                    <input type = "password" id = "Password" name = "Password">
-                                </div>
-                                <div>
-                                    <label for = "repeat_password" >Repeat Password</label>
-                                    <input type = "password" id = "repeat_password" name = "repeat_password">
-                                </div>
-                                <div>
-                                    <button>Sign up</button>
-                                </div>
-                            </form>
+        <br>
+	Hello <?php echo $user_data['name']; ?>  <br> <br>
+    Age : <?php echo $user_data['age']; ?>   <br> <br>
+    SSN : <?php echo $user_data['SSN']; ?>   <br> <br>
+    Address : <?php echo $user_data['address'];?>   <br> <br>
+    <a href="logout.php">Logout</a>
 
-                        </div>
+    <br> <br> <br>
 
-                    </div>
-                </div>
-            </div>
+    
 
-                                   
-        </body>
+    </body>
     </html>
